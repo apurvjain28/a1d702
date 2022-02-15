5a6efa7919cc4f6b905768ba223151481cdf7dbd
ajain4743 <apurvjain28@gmail.com>
2022-02-15 18:05:44 -0500

    model-changes

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,14 +4,14 @@ const Message = require("./message");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+User.belongsToMany(Conversation, { through: "User_Conversation" });
+Conversation.belongsToMany(User, { through: "User_Conversation" });
 Message.belongsTo(Conversation);
+Message.hasMany(User);
 Conversation.hasMany(Message);
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
 };
